7bbefad9dcda8288518da0ad2dc8c4594d3bccf7
Arnaldo Castelo <140561779+ArnaldoCastelo@users.noreply.github.com>
2026-09-04 20:55:04 -0300

    Implement basic calculator functionality in Python
    codigo fonte!

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+
 print("Minha primeira calculadora em Python")
+print("=== calculadora ===")
+
+numero1 = float(input("Digite o primeiro numero: "))
+operacao = input("Digite a operação (+, -, * ou /): ")
+numero2 = float(input("Digite o segundo numero: "))
+
+if operacao == "+":
+    resultado = numero1 + numero2
+
+elif operacao == "-":
+    resultado = numero1 - numero2
+
+elif operacao == "*":
+    resultado = numero1 * numero2
+
+elif operacao == "/":
+    if numero2 != 0:
+        resultado = numero1 / numero2
+    else:
+        resultado = "Não é possível dividir por zero."
+
+else:
+    resultado = "Operação inválida."
+
+print("Resultado:", resultado)
